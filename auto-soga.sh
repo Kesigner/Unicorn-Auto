48ec3b980702be63c72803ab99f7913f56a9fc33
auki <499417656@qq.com>
2022-06-13 00:24:03 +0800

    fix

--- auto-soga.sh
+++ auto-soga.sh
@@ -7,19 +7,29 @@ fi
 
 
 
-download_trojan(){
+install_soga(){
 	echo "正在安装soga . . ."
 	bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/soga/master/install.sh)
 	rm -f /etc/soga/soga.conf
 	rm -f /etc/soga/blockList
-	wget -P /etc/soga https://raw.githubusercontent.com/Kesigner/unicorn/main/unicorn-config/trojan-soga.conf -O soga.conf
-	wget -P /etc/soga https://raw.githubusercontent.com/Kesigner/unicorn/main/unicorn-config/blockList
-    echo -e "已添加soga审计"
+    echo -e "soga安装完毕，初始化环境已就绪！>> 进入soga初始化对接！"
+	shon_online
+}
+
+download_trojan(){
+	echo "开始安装trojan配置文件 . . ."
+	wget -P /etc/soga https://raw.githubusercontent.com/Kesigner/Unicorn-Auto/main/conf/trojan-soga.conf -O soga.conf
+    echo -e "trojan配置文件已下发完成！"
+	wget -P /etc/soga https://raw.githubusercontent.com/Kesigner/Unicorn-Auto/main/BlockList
+    echo -e "soga审计规则已下发完成！"
+    wget -P /etc/soga https://raw.githubusercontent.com/Kesigner/Unicorn-Auto/main/certificates/ip172666023.mobgslb.tbcache.com_chain.crt
+    wget -P /etc/soga https://raw.githubusercontent.com/Kesigner/Unicorn-Auto/main/certificates/ip172666023.mobgslb.tbcache.com_key.key
+    echo -e "TLS证书配置已下发完成！"
+    echo "开始进行soga对接 . . ."
 	cd /etc/soga
 	printf "请输入节点ID："
 	read -r nodeId <&1
 	sed -i "s/ID_HERE/$nodeId/" soga.conf
-    ask_if
     soga restart
 	echo -e "正在重启soga服务端！"
 	shon_online
@@ -29,7 +39,7 @@ add_shenji(){
 	echo "正在添加审计 . . ."
     	rm -f /etc/soga/blockList
     	wget -P /etc/soga https://raw.githubusercontent.com/Kesigner/unicorn/main/unicorn-config/blockList
-        echo -e "已添加soga审计"
+        echo -e "soga审计规则已下发完成！"
 	shon_online
 }
 
@@ -48,24 +58,26 @@ restart_unicorn(){
 shon_online(){
 echo "请选择您需要进行的操作:"
 echo "1) 安装 soga"
-echo "2) 启动 soga"
-echo "3) 重启 soga"
-echo "4) 查看 soga状态"
-echo "5) 查看 soga日志"
-echo "6) 添加审计"
-echo "7) 退出脚本"
-echo "   Version：2.0.0"
+echo "2) 配置 trojan"
+echo "3) 启动 soga"
+echo "4) 重启 soga"
+echo "5) 查看 soga状态"
+echo "6) 查看 soga日志"
+echo "7) 添加审计"
+echo "8) 退出脚本"
+echo "   Version：3.0.0"
 echo ""
 echo -n "   请输入编号: "
 read N
 case $N in
-  1) download_unicorn ;;
-  2) start_unicorn ;;
-  3) restart_unicorn ;;
-  4) soga status ;;
-  5) soga log ;;
-  6) add_shenji ;;
-  7) exit ;;
+  1) install_soga ;;
+  2) download_trojan ;;
+  3) start_unicorn ;;
+  4) restart_unicorn ;;
+  5) soga status ;;
+  6) soga log ;;
+  7) add_shenji ;;
+  8) exit ;;
   *) echo "Wrong input!" ;;
 esac 
 }
