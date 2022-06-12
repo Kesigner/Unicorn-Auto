7a9a61690f4b0ba2d2d2e21eed2c74c3dde045c8
auki <499417656@qq.com>
2022-06-13 01:26:28 +0800

    Update auto-soga.sh

--- auto-soga.sh
+++ auto-soga.sh
@@ -44,7 +44,7 @@ download_trojan(){
     echo -e "${green}开始进行soga对接 . . .${plain}"
     echo ""
 	cd /etc/soga
-	printf "${green}请输入节点ID： ${plain}"
+	printf "${green}请输入节点ID：${plain}"
 	read -r nodeId <&1
 	sed -i "s/ID_HERE/$nodeId/" soga.conf
     soga restart
